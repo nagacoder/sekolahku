5757276f503952ededcfa105f1ed6fa336a27b93
nagacoder <nagacoder@outlook.com>
2022-12-19 19:13:59 +0700

    fix: remove base url

--- sekolah-docusaurus/docusaurus.config.js
+++ sekolah-docusaurus/docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: "Sekolahku",
   tagline: "Dinosaurs are cool",
   url: "https://sekolaku-docusaurus.netlify.app",
-  baseUrl: "/sekolah-docusaurus",
+  baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
